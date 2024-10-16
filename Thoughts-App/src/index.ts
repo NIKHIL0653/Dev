e27977d37fcbd88a1c2554968637055758ee914f
Nikhil Choudhary <nikhil0653@gmail.com>
2024-10-16 22:53:08 +0530

    app

--- Thoughts-App/src/index.ts
+++ Thoughts-App/src/index.ts
@@ -2,8 +2,35 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+//we need file based routing for the app
+
+app.post('/api/v1/user/signup', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/user/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/user/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/api/v1/user/blog/blog', (c) => {
   return c.text('Hello Hono!')
 })
 
 export default app
+
+//but we also need prisma to create a connection pool to access the database
+//since we are creting a serverless application so it will create multiple instances of an application around the world
+//it is wrong approach when multiple mini machines connect to db it is good practice to connect to a connection pool first.
+
