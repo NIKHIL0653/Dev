11aee990f71624f62c6231699492a541b7f12066
Nikhil Choudhary <nikhil0653@gmail.com>
2024-12-18 21:50:11 +0530

    react project

--- Javascript/React/Padre Ginos/src/App.js
+++ Javascript/React/Padre Ginos/src/App.js
@@ -1,11 +1,19 @@
-const App = () => {
-    return React.createElement(
-      "div",
-      {},
-      React.createElement("h1", {}, "Homemade Food Anytime")
-    );
+const Pizza = () => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "The Pepperoni Pizza"),
+      React.createElement("p", {}, "Mozzarella Cheese, Pepperoni"),
+    ]);
   };
-  
-  const container = document.getElementById("root");
-  const root = ReactDOM.createRoot(container);
-  root.render(React.createElement(App));
+
+  const App = () => {
+    return React.createElement("div", {}, [
+      React.createElement("h1", {}, "Pixel Perfect Pizzas"),
+      React.createElement(Pizza),
+      React.createElement(Pizza),
+      React.createElement(Pizza),
+    ]);
+  };
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
